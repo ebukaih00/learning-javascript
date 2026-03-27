71edce8103cff37883706c3ae9f0c5904fa123fd
Chukwuebuka ihueze <ihuezeebuka5@gmail.com>
2026-03-27 00:49:46 +0000

    Initial setup of JavaScript practice project with welcome message functionality

---
+++ index.js
@@ -0,0 +1,56 @@
+// let count = 0 
+
+// let countEl = document.getElementById("count-el")
+
+// function increment(){
+//     count = count + 1
+//     countEl.innerText = count
+//     console.log(count)
+// }
+
+
+// function saveCount(){
+//     console.log(count)
+// }
+
+// let user = "ebuka"
+
+// let greeting = "Holla, my name is"
+
+// let salutation = (greeting + "," + " "  + user)
+
+//     console.log(salutation)
+
+
+// let age = 25
+
+// let gender = " man"
+
+// let intro = age + gender
+
+// console.log(intro)
+
+
+// let dog = "A very big dog, a \"dangerous dog\""
+
+// console.log(dog)
+
+
+
+
+let welcomeEl = document.getElementById("welcome-el")
+
+// Create two variables (name & greeting) that contains your name
+// and the greeting we want to render on the page
+
+let name = "Per Harald Borgen"
+let greeting = "Welcome back"
+
+// Render the welcome message using welcomeEl.innerText
+welcomeEl.innerText = greeting + name
+
+
+
+
+
+
